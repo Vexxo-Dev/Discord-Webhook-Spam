c6a03a4c8e984674759c28988def7cb0d79c8873
Yassin Ahmed <75578911+Vexxo-Dev@users.noreply.github.com>
2021-11-30 11:48:51 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+import time
+
+import requests
+
+import pyfiglet
+
+import os
+
+os.system("pip install pyfiglet")
+
+os.system('clear')
+
+os.system("pyfiglet --font=5lineoblique --color=GREEN Vexxo")
+
+msg = input("Webhook Message -> ")
+
+webhook = input("Webhook Link -> ")
+
+def spam(msg, webhook):
+    while True:
+        try:
+            data = requests.post(webhook, json={'content': msg})
+            if data.status_code == 204:
+                print(f"Sent Msg {msg}")
+        except:
+            print("Bad Webhook ->" + webhook)
+            time.sleep(5)
+            exit()
+
+vexxo_top = 1
+
+while vexxo_top == 1:
+    spam(msg, webhook)
